74dc9ee9c5112f6b0e4125f73d555aaafa516e60
vagrant <vagrant@acontroller-192-168-56-95.nip.io>
2022-02-27 21:29:54 +0000

    test

--- ansible-oracle/full_install_single_instance.groovy
+++ ansible-oracle/full_install_single_instance.groovy
@@ -1,7 +1,7 @@
 multibranchPipelineJob('ansible-oracle-dbfs-full') {
   branchSources {
     git {
-        // id('123000001') // IMPORTANT: use a constant and unique identifier
+        id('123000001') // IMPORTANT: use a constant and unique identifier
         remote('https://github.com/Rendanic/ansible-oracle-inventory.git')
     }
   }
